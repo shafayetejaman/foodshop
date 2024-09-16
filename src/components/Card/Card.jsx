3965582491442860d13efc7e352bd008e2b67443
shafayetejaman <shafayetejaman@gmail.com>
2024-09-17 01:57:24 +0600

    Initial commit

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { FaUtensils, FaWeight, FaTags, FaReact } from 'react-icons/fa';
+import { IoEarth } from "react-icons/io5";
+import { Link } from 'react-router-dom';
+
+const Card = ({ cardData }) =>
+{
+
+    return (
+        <div className="overflow-hidden rounded-lg shadow-xl transition-transform duration-300 ease-in-out transform hover:scale-105"
+            style={{
+                background: 'linear-gradient(to bottom right, #1a202c, #2d3748)'
+            }}>
+            <img
+                className="w-full h-48 object-cover"
+                src={cardData.image}
+                alt={cardData.name}
+            />
+            <div className="px-6 py-4">
+                <h2 className="mb-2 text-2xl font-bold text-white">{cardData.name}</h2>
+                <div className="flex justify-between items-center mt-3">
+                    <div>
+                        <p className="mb-2 text-sm text-gray-300 flex items-center">
+                            <FaUtensils className="mr-2" />
+                            {cardData.category}
+                        </p>
+                        <p className="mb-2 text-sm text-gray-300 flex items-center">
+                            <IoEarth className="mr-2" />
+                            {cardData.country}
+                        </p>
+                    </div>
+                    <Link to={`/details/${cardData.idMeal}`} className="btn btn-sm btn-white text-lg">Detail</Link>
+
+                </div>
+            </div>
+            <div className="px-6 pt-4 pb-2">
+                <div className="flex items-center mb-3">
+                    <FaTags className="mr-2 text-gray-300" />
+                    <span className="text-sm text-gray-300">Tags:</span>
+                </div>
+                {cardData.tags?.map((tag, idx) => (
+                    <span
+                        key={idx}
+                        className="inline-block px-3 py-1 mb-2 mr-2 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full hover:bg-gray-300 transition-colors duration-200"
+                    >
+                        {tag}
+                    </span>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Card;
---
+++ src/components/MainSection/Cards/Cards.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import Card from "../../Card/Card";
+import { getMeal, useAPI } from "../../../context/api/api";
+import { useNavigate, useSearchParams } from 'react-router-dom';
+
+const MAX_PAGE_ITEMS = 12;
+
+export default function Cards()
+{
+    const [cardList, setCardList] = useState(null);
+    const [pageNumber, setPageNumber] = useState(1);
+    const { meals } = useAPI();
+    const navigate = useNavigate();
+
+    useEffect(() =>
+    {
+        (async () =>
+        {
+            if (!meals) return;
+
+            const data = await Promise.all(meals.map(async (meal) =>
+            {
+                const mealData = await getMeal(meal?.idMeal);
+                return {
+                    name: mealData?.strMeal,
+                    category: mealData?.strCategory,
+                    country: mealData?.strArea,
+                    tags: mealData?.strTags?.split(','),
+                    image: mealData?.strMealThumb
+                };
+            }));
+
+            setCardList(data);
+
+        })();
+    }, [meals]);
+
+    useEffect(() =>
+    {
+        navigate(`/?page=${pageNumber}`);
+
+    }, [pageNumber]);
+
+    const start = (pageNumber - 1) * MAX_PAGE_ITEMS;
+    const end = start + MAX_PAGE_ITEMS;
+
+
+    return (
+        <section className='my-24 md:mx-16 mx-8'>
+            {!cardList && <div className='flex justify-center my-20'><span className="loading loading-spinner loading-lg"></span></div>}
+            <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-10'>
+                {cardList?.slice(start, end).map((meal, idx) => <Card key={idx} cardData={meal}></Card>)}
+            </div>
+            <div className="mt-12 flex justify-center gap-2">
+                {cardList && <>
+                    <button className={`${pageNumber <= 1 ? "btn-disabled" : 'btn-info'} btn-sm font-bold rounded-md btn-outline border-3 border-solid transition-transform duration-300 ease-in-out transform hover:scale-110`}
+                        onClick={() => setPageNumber(pageNumber - 1)}>&lt; &lt; Previous Page</button>
+                    <div className="px-2 border-solid font-bold rounded-md text-red-500">{pageNumber}</div>
+                    <button className={`${Math.ceil(cardList.length / MAX_PAGE_ITEMS) <= pageNumber ? "btn-disabled" : 'btn-success'} font-bold btn-sm rounded-md btn-outline border-3 border-solid transition-transform duration-300 ease-in-out transform hover:scale-110`}
+                        onClick={() => setPageNumber(pageNumber + 1)}>Next Page &gt;&gt;</button>
+                </>}
+            </div>
+        </section>
+    );
+}
